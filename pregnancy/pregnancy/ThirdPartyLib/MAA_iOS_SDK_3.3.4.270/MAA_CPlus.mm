d403152fae009c059b7aeaa14193cc986c76c577
liumiao <liumuao1988@gmail.com>
2014-10-22 11:34:03 +0800

    delete extra files

--- pregnancy/pregnancy/ThirdPartyLib/MAA_iOS_SDK_3.3.4.270/MAA_CPlus.mm
+++
@@ -1,9 +0,0 @@
-//
-//  MAA_C++.cpp
-//  pregnancy
-//
-//  Created by liumiao on 6/24/14.
-//  Copyright (c) 2014 babytree. All rights reserved.
-//
-
-
--- pregnancy/pregnancy/ThirdPartyLib/inc/MapTest.mm
+++
@@ -1,7 +0,0 @@
-//
-//  MapTest.cpp
-//  pregnancy
-//
-//  Created by Wang Jun on 12-11-2.
-//  Copyright (c) 2012年 babytree. All rights reserved.
-//
